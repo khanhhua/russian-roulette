7a18bf1a81653a29d84c78e115aa77c556fa2503
Khanh Hua <khanh.hua@doctari.de>
2022-04-29 14:56:27 +0200

    Client interaction

--- static/script.js
+++ static/script.js
@@ -1,1 +1,27 @@
-console.log('You rocks!');
+window.onload = init;
+
+function init() {
+    const $replOutput = document.getElementById('replOutput');
+    const $cmdInput = document.getElementById('cmdInput')
+    cmdInput.addEventListener('change', (e) => execute($cmdInput, $replOutput, e.target.value));
+}
+
+function appendToRepl($replOutput, text) {
+    const child = document.createElement('p');
+    child.textContent = text;
+    $replOutput.appendChild(child);
+}
+
+function execute($cmdInput, $replOutput, cmd) {
+    appendToRepl($replOutput, cmd);
+
+    $cmdInput.value = ''
+
+    const [method, parameter] = cmd.split(' ');
+    fetch('/', {
+        method,
+        body: parameter
+    })
+    .then(res => res.text())
+    .then(text => appendToRepl($replOutput, text))
+}
